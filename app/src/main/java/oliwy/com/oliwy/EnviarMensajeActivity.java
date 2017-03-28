e343f40f9cc87f0044617d3ad8718bf680822cd6
NiraBH <nirabethencourt@gmail.com>
2017-03-28 10:20:51 +0200

    Initial commit

---
+++ app/src/main/java/oliwy/com/oliwy/EnviarMensajeActivity.java
@@ -0,0 +1,13 @@
+package oliwy.com.oliwy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnviarMensajeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_mensaje);
+    }
+}
